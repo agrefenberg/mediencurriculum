8f53cbfe672ceefaf8df5ac8f863ee5ab5b42edb
agrefenberg <42937200+agrefenberg@users.noreply.github.com>
2026-06-22 17:11:30 +0200

    Add files via upload

---
+++ lesestoff.js
@@ -0,0 +1,55 @@
+// lesestoff.js - Externe Datenbank für die rollierenden Lesekisten
+window.lesekistenPool = {
+    5: [
+        { 
+            id: "b5_rico", 
+            titel: "Rico, Oskar und die Tieferschatten", 
+            autor: "Andreas Steinhöfel", 
+            seitenMax: 224,
+            genre: "Krimi / Freundschaft",
+            linkMaterial: "https://www.dtv.de/lehrer/lehrerpruefstuecke/schullektuere-realschule"
+        },
+        { 
+            id: "b5_vorstadt", 
+            titel: "Vorstadtkrokodile", 
+            autor: "Max von der Grün", 
+            seitenMax: 160,
+            genre: "Jugendbuch-Klassiker",
+            linkMaterial: ""
+        },
+        { 
+            id: "b5_wunder", 
+            titel: "Wunder", 
+            autor: "Raquel J. Palacio", 
+            seitenMax: 384,
+            genre: "Inklusion / Empathie",
+            linkMaterial: "https://www.dfg-kuw.de/lesematerial-wunder"
+        }
+    ],
+    6: [
+        { 
+            id: "b6_emil", 
+            titel: "Emil und die Detektive", 
+            autor: "Erich Kästner", 
+            seitenMax: 176,
+            genre: "Klassiker / Detektivgeschichte",
+            linkMaterial: "https://www.kaestner-gesellschaft.de/material"
+        },
+        { 
+            id: "b6_krabat", 
+            titel: "Krabat", 
+            autor: "Otfried Preußler", 
+            seitenMax: 256,
+            genre: "Mystery / Fantastisch",
+            linkMaterial: ""
+        },
+        { 
+            id: "b6_level4", 
+            titel: "Level 4 - Stadt der Kinder", 
+            autor: "Andreas Schlüter", 
+            seitenMax: 272,
+            genre: "Medienreflexion / Abenteuer",
+            linkMaterial: ""
+        }
+    ]
+};
