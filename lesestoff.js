c611d601f45c6f6473e725161c002e150e041453
agrefenberg <42937200+agrefenberg@users.noreply.github.com>
2026-06-22 21:06:40 +0200

    Add files via upload

--- lesestoff.js
+++ lesestoff.js
@@ -9,21 +9,29 @@ window.lesekistenPool = {
             genre: "Krimi / Freundschaft",
             linkMaterial: "https://www.dtv.de/lehrer/lehrerpruefstuecke/schullektuere-realschule"
         },
-        { 
-            id: "b5_vorstadt", 
-            titel: "Vorstadtkrokodile", 
-            autor: "Max von der Grün", 
-            seitenMax: 160,
-            genre: "Jugendbuch-Klassiker",
+        {
+            id: "b5_master",
+            titel: "Master of Disaster - Chaos ist mein zweiter Name",
+            autor: "Stephan Knösel",
+            seitenMax: 192,
+            genre: "Humor / Schulalltag",
             linkMaterial: ""
         },
-        { 
-            id: "b5_wunder", 
-            titel: "Wunder", 
-            autor: "Raquel J. Palacio", 
-            seitenMax: 384,
-            genre: "Inklusion / Empathie",
-            linkMaterial: "https://www.dfg-kuw.de/lesematerial-wunder"
+        {
+            id: "b5_ritter",
+            titel: "Julia und die Stadtteilritter",
+            autor: "Antje Herden",
+            seitenMax: 176,
+            genre: "Abenteuer / Freundschaft",
+            linkMaterial: ""
+        },
+        {
+            id: "b5_fenimore",
+            titel: "Fenimore Castle - A mystery for the stage",
+            autor: "Dave Lambert",
+            seitenMax: 64,
+            genre: "Englische Lektüre / Zeitreise",
+            linkMaterial: ""
         }
     ],
     6: [
@@ -35,20 +43,80 @@ window.lesekistenPool = {
             genre: "Klassiker / Detektivgeschichte",
             linkMaterial: "https://www.kaestner-gesellschaft.de/material"
         },
-        { 
-            id: "b6_krabat", 
-            titel: "Krabat", 
-            autor: "Otfried Preußler", 
-            seitenMax: 256,
-            genre: "Mystery / Fantastisch",
+        {
+            id: "b6_schatten",
+            titel: "Schattenkinder",
+            autor: "Margaret Peterson Haddix",
+            seitenMax: 192,
+            genre: "Dystopie / Spannung",
             linkMaterial: ""
         },
-        { 
-            id: "b6_level4", 
-            titel: "Level 4 - Stadt der Kinder", 
-            autor: "Andreas Schlüter", 
+        {
+            id: "b6_caesar",
+            titel: "Ich, Caesar, und die Bande vom Kapitol",
+            autor: "Frank Schwieger",
+            seitenMax: 240,
+            genre: "Geschichte / Humor",
+            linkMaterial: ""
+        },
+        {
+            id: "b6_charlie",
+            titel: "Charlie Joe Jackson - Lesen verboten",
+            autor: "Tommy Greenwald",
+            seitenMax: 224,
+            genre: "Humor / Antileser-Mythen",
+            linkMaterial: ""
+        }
+    ],
+    7: [
+        {
+            id: "b7_kontrolle",
+            titel: "Außer Kontrolle - Wenn aus Spiel bitterer Ernst wird",
+            autor: "Bali Rai",
+            seitenMax: 128,
+            genre: "Krimi / Jugendkriminalität",
+            linkMaterial: ""
+        },
+        {
+            id: "b7_planet",
+            titel: "Es ist dein Planet - Ideen gegen den Irrsinn",
+            autor: "Sascha Mamczak",
+            seitenMax: 256,
+            genre: "Sachbuch / Ökologie & Zukunft",
+            linkMaterial: ""
+        },
+        {
+            id: "b7_finn",
+            titel: "Finn Black - Der falsche Deal",
+            autor: "Kevin Brooks",
             seitenMax: 272,
-            genre: "Medienreflexion / Abenteuer",
+            genre: "Thriller / Familie",
+            linkMaterial: ""
+        }
+    ],
+    8: [
+        {
+            id: "b8_rotblau",
+            titel: "Rot oder blau",
+            autor: "Manfred Theisen",
+            seitenMax: 192,
+            genre: "Politik-Thriller / Demokratie",
+            linkMaterial: ""
+        },
+        {
+            id: "b8_johnny",
+            titel: "Johnny Delgado - Im freien Fall",
+            autor: "Kevin Brooks",
+            seitenMax: 288,
+            genre: "Detektiv / Gang-Milieu",
+            linkMaterial: ""
+        },
+        {
+            id: "b8_george",
+            titel: "Alle lieben George - keiner weiß wieso",
+            autor: "Frank Cottrell Boyce",
+            seitenMax: 240,
+            genre: "Humor / Identität",
             linkMaterial: ""
         }
     ]
